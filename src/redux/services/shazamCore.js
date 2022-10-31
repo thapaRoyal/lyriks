b82c3e3cb401146631ca58fb1de9b512c8133a31
thapaRoyal <thaparoyal17@gmail.com>
2022-10-31 11:54:39 -0400

    lyrics

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -14,7 +14,10 @@ export const shazamCoreApi = createApi({
   }),
   endpoints: (builder) => ({
     getTopCharts: builder.query({ query: () => '/charts/world' }),
+    getSongDetails: builder.query({
+      query: ({ songid }) => `/tracks/details?track_id=${songid}`,
+    }),
   }),
 });
 
-export const { useGetTopChartsQuery } = shazamCoreApi;
+export const { useGetTopChartsQuery, useGetSongDetailsQuery } = shazamCoreApi;
